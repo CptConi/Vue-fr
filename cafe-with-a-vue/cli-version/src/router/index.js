85ee14b85d3bf2a36b61dd6c5a2cab0c4487c718
CptConi <nicolas.renard.33@gmail.com>
2020-08-24 11:22:35 +0200

    P3C1

--- cafe-with-a-vue/cli-version/src/router/index.js
+++ cafe-with-a-vue/cli-version/src/router/index.js
@@ -5,30 +5,29 @@ import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+  const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: Home
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/contact',
-    name: 'contact',
+    name: 'Contact',
     component: Contact
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: history,
   routes
 })
 
--- cafe-with-a-vue/cli-version/src/views/Home.vue
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<h1>{{ restaurantName }}</h1>
+  <div id="app">
+    <h1>{{ restaurantName }}</h1>
 		<p class="description">
 			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
 			notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
@@ -10,85 +10,99 @@
 
 		<section class="menu">
 			<h2>Menu</h2>
-			<MenuItem
-				v-for="item in simpleMenu"
-				:addToShoppingCart="addToShoppingCart"
-				:name="item.name"
-				:image="item.image"
-				:price="item.price"
-				:quantity="item.quantity"
-				:inStock="item.inStock"
-				:key="item.name"
-			/>
+			<div v-for="item in simpleMenu" :key="item.name" class="menu-item">
+				<img
+					class="menu-item__image"
+					:src="item.image.source"
+					:alt="item.image.alt"
+				/>
+				<div>
+					<h3>{{ item.name }}</h3>
+					<p v-if="item.inStock">En stock</p>
+					<p v-else>En rupture de stock</p>
+					<div>
+						<label for="add-item-quantity"
+							>Quantité : {{ item.quantity }}</label
+						>
+						<input
+							v-model.number="item.quantity"
+							id="add-item-quantity"
+							type="number"
+						/>
+						<button @click="addToShoppingCart(item.quantity)">
+							Ajouter au panier
+						</button>
+					</div>
+				</div>
+			</div>
 		</section>
 
-		<div class="shopping-cart">
-			<h2>Panier : {{ shoppingCart }} articles</h2>
-		</div>
-
-		<footer class="footer">
-			<p>{{ copyright }}</p>
-		</footer>
-	</div>
+		<aside class="shopping-cart">
+			<h2>Panier d'achat : {{ shoppingCart }} articles</h2>
+		</aside>
+  </div>
 </template>
 
-<script>
-import MenuItem from "../components/MenuItem"
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+
+<script>
 export default {
-	name: "Home",
-	components: {
-		MenuItem
-	},
-	data() {
+  name: "Home",
+  data() {
 		return {
 			restaurantName: "La belle vue",
-			shoppingCart: 0,
-			simpleMenu: [
-				{
-					name: "Croissant",
-					image: {
-						source: "/images/crossiant.jpg",
-						alt: "Un croissant"
+				shoppingCart: 0,
+				simpleMenu: [
+					{
+						name: "Croissant",
+						image: {
+							source: "/images/crossiant.jpg",
+							alt: "Un croissant"
+						},
+						inStock: true,
+						quantity: 1
 					},
-					inStock: true,
-					quantity: 1,
-					price: 2.99
-				},
-				{
-					name: "Baguette de pain",
-					image: {
-						source: "/images/french-baguette.jpeg",
-						alt: "Quatre baguettes de pain"
+					{
+						name: "Baguette de pain",
+						image: {
+							source: "/images/french-baguette.jpeg",
+							alt: "Quatre baguettes de pain"
+						},
+						inStock: true,
+						quantity: 1
 					},
-					inStock: true,
-					quantity: 1,
-					price: 3.99
-				},
-				{
-					name: "Éclair",
-					image: {
-						source: "/images/eclair.jpg",
-						alt: "Éclair au chocolat"
-					},
-					inStock: false,
-					quantity: 1,
-					price: 4.99
-				}
-			]
-		}
-	},
-	computed: {
-		copyright() {
-			const currentYear = new Date().getFullYear()
-
-			return `Copyright ${this.restaurantName} ${currentYear}`
-		}
-	},
-	methods: {
-		addToShoppingCart(amount) {
-			this.shoppingCart += amount
+					{
+						name: "Éclair",
+						image: {
+							source: "/images/eclair.jpg",
+							alt: "Éclair au chocolat"
+						},
+						inStock: false,
+						quantity: 1
+					}
+				]
 		}
 	}
-}
+};
 </script>
